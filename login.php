b7328505f700fa8dc7bbed2c8037171bf871ac2e
Neal Bhalodia <nealbhalodia@gmail.com>
2020-11-25 16:58:59 -0500

    login update

--- login.php
+++ login.php
@@ -18,8 +18,7 @@ if (isset($_POST['uname']) && isset($_POST['password'])) {
 	   $data = htmlspecialchars($data);
 	   return $data;
 	}
-
-	$uname = validate($_POST['uname']);
+    $uname = validate($_POST['uname']);
 	$pass = validate($_POST['password']);
 
 	if (empty($uname)) {
@@ -29,12 +28,17 @@ if (isset($_POST['uname']) && isset($_POST['password'])) {
         header("Location: index.php?error=Password is required");
 	    exit();
 	}else{
-		$sql = "SELECT Id FROM members WHERE username='{$_POST[uname]}' AND password='{$_POST[password]}';";
+		$sql = "SELECT * FROM members WHERE username='$uname' AND password='$pass';";
 
-		if ($conn->query($sql) === TRUE){
-                echo "Login success...";
-                $_SESSION['loggedin']=TRUE;
-                header('Location: collection.php');
+		$result = mysqli_query($conn, $sql);
+
+        if (mysqli_num_rows($result) === 1) {
+        	$row = mysqli_fetch_assoc($result);
+            if ($row['username'] === $uname && $row['password'] === $pass) {
+                $_SESSION['user_name'] = $row['username'];
+                $_SESSION['id'] = $row['id'];
+                header("Location: collection.php");
+        		exit();
 		}else{
 			header("Location: index.php?error=Incorect User name or password");
 	        exit();
