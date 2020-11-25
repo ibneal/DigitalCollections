7839d615682efbe4908d2d419a7693773ec468f2
Neal Bhalodia <nealbhalodia@gmail.com>
2020-11-25 17:16:26 -0500

    New base code

--- login.php
+++ login.php
@@ -1,6 +1,6 @@
 <?php 
 
-$sname= "aqx5w9yc5brambgl.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+$sname= "aqx5w9yc5brambgl.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/";
 $unmae= "asv8nlrt3ji7v1ee";
 $password = "zafjp7fo15x2qsek";
 $db_name = "m13a7advxe1eiscn";
