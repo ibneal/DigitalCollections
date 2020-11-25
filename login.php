1f652f9eda1c5a7f09c61fa1ff674d8d9fef40fd
Neal Bhalodia <nealbhalodia@gmail.com>
2020-11-25 17:27:22 -0500

    New base code

--- login.php
+++ login.php
@@ -1,24 +1,24 @@
 <?php 
 
-$sname= "aqx5w9yc5brambgl.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/";
+$sname= "aqx5w9yc5brambgl.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
 $unmae= "asv8nlrt3ji7v1ee";
 $password = "zafjp7fo15x2qsek";
 $db_name = "m13a7advxe1eiscn";
-
-$con = mysqli_connect($sname, $unmae, $password, $db_name);
-
-if(! $con)
-{
-    die('Connection Failed'.mysql_error());
-}
 
 // Grab User submitted information
 $username = $_POST["users_name"];
 $pass = $_POST["users_pass"];
 
+// Connect to the database
+$con = mysql_connect("aqx5w9yc5brambgl.cbetxkdyhwsb.us-east-1.rds.amazonaws.com","asv8nlrt3ji7v1ee","zafjp7fo15x2qsek");
+// Make sure we connected successfully
+if(! $con)
+{
+    die('Connection Failed'.mysql_error());
+}
 
 // Select the database to use
-mysql_select_db("m13a7advxe1eiscn",$con);
+mysql_select_db("m13a7advxe1eiscn",$con)
 
 $result = mysql_query("SELECT username, password FROM members WHERE username = $username");
 
