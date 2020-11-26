55f47db8fa5b945dce5aeaaed497e7c82790bd8f
Neal Bhalodia <nealbhalodia@gmail.com>
2020-11-25 20:40:42 -0500

    check fix

--- check.php
+++ check.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-if(!($_SESSION['loggedin']) == True ))
+if(($_SESSION['loggedin']) != True ))
 {
     header("Location: https://digital-collections.herokuapp.com/index.php")
 }
