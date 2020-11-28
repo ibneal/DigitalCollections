9b64e4c739ff32c05c97cacb8bf95774b3a39db3
Neal Bhalodia <nealbhalodia@gmail.com>
2020-11-27 20:20:27 -0500

    add item to collection

---
+++ addItem.php
@@ -0,0 +1,55 @@
+<?php
+session_start();
+include 'database.php';
+if(!$_SESSION['loggedin']){
+    header('Location: index.php'); }
+$current_user = $_SESSION['username'];
+
+ //Put session start at the beginning of the file
+?>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Brew457 - Employees</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="./css/bootstrap.min.css">
+    <link rel="stylesheet" href="./css/style.css">
+  </head>
+  <body>
+    <ul class="topnav">
+     <?php
+                   if ($_SESSION['loggedin']==True){
+                     echo '<li><a href="./collection.php">Collections</a></li>';
+                   }?>
+                   <?php
+                         if ($_SESSION['loggedin']==True){
+                           echo '<li><a href="./addItem.php">Add Item</a></li>';
+                         }?>
+             <li><a href="./contact.php">About Us</a></li>
+             <li> <button><a href="logout.php">Logout</a></button></li>
+    </ul>
+
+<form action="addItem_helper.php" method="post">
+<div class="form-group">
+ <label for="item_name"><b>Item Name</b></label>
+    <input type="text" class="form-control" placeholder="Enter item name" name="item_name" required>
+  </div>
+  <div class="form-group">
+   <label for="item_location"><b>Username</b></label>
+      <input type="text" class="form-control" placeholder="Enter item location if applicable" name="item_location" required>
+    </div>
+    <div class="form-group">
+    <label for="item_notes"><b>Item notes</b></label>
+    <input type="text" class="form-control" placeholder="Enter item notes" name="item_notes" required>
+</div>
+    <button type="submit" class="btn btn-primary">Add item</button>
+</form>
+
+  </body>
+</html>';
+}
+else{
+  echo '<p>Access to this page is forbidden.</p><br><p>Did you mean to <a href="./login.php">login?</a></p>';
+}
---
+++ addItem_helper.php
@@ -0,0 +1,31 @@
+<?php
+    session_start();
+    $url = getenv('JAWSDB_URL');
+    $dbparts = parse_url($url);
+    $hostname = $dbparts['host'];
+    $username = $dbparts['user'];
+    $password = $dbparts['pass'];
+    $database = ltrim($dbparts['path'],'/');
+
+    $current_user = $_SESSION['username'];
+    $item_name = $_POST['item_name'];
+    $item_location = $_POST['item_location'];
+    $item_notes = $_POST['item_notes']
+
+    $conn=new mysqli($hostname,$username,$password,$database);
+
+    if($conn->connect_error){
+        die("Connection failed: " . $conn->connect_error);
+    }
+    $sql = "INSERT INTO collection (userId, name, location, notes) VALUES ('$current_user', '$item_name', '$item_location', '$item_notes') ";
+
+    $retval = mysql_query($sql,$conn);
+
+       if(! $retval ) {
+          die('Could not enter data: ' . mysql_error());
+       }
+
+       echo "Entered data successfully\n";
+
+       mysql_close($conn);
+?>
