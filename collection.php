9ae565a8e39b2fb0bff94fa7d9f76b109c1ede58
Neal Bhalodia <nealbhalodia@gmail.com>
2020-11-30 17:29:29 -0500

    center login and collection add item link update.

--- collection.php
+++ collection.php
@@ -33,7 +33,7 @@ if(!$_SESSION['loggedin']){
       <div class="row">
         <div class="col-md"></div>
         <div class="col-md-9">
-        <button class="signupbtn" onclick="document.getElementById('id03').style.display='block'">Add Item</button>
+        <button class="signupbtn" onclick="location.href = 'https://digital-collections.herokuapp.com/addItem.php';">Add Item</button>
         <button class="cancelbtn" onclick="document.getElementById('id02').style.display='block'">Remove Item</button>
         <button class="logbutton" onclick="document.getElementById('id03').style.display='block'">Update Item</button>
         <input type="text" id="search" onkeyup="search()" placeholder="Search for items.." title="Type in a name">
